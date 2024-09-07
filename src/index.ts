46e57775d66ae75abb96d7d2be2cad788e8a3a61
catinrage <catinrage@gmail.com>
2024-09-07 13:09:55 +0330

    Refactor cheeze function to accept options parameter

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,36 @@
 import { BasicObservable, InputElementObservable } from './observable';
-import type { DOMElements, ObservableValueOptions } from './types';
+import type {
+  ObservableDOMElements,
+  ObservableOptions,
+  ObservableValueOptions,
+} from './types';
 
-function isDOMInputElement(value: unknown): value is DOMElements {
+function isDOMInputElement(value: unknown): value is ObservableDOMElements {
+  // Check if running in a browser environment
+  const isBrowser = typeof window !== 'undefined';
+  if (!isBrowser) {
+    return false; // Not in a browser environment, return false
+  }
   return (
     value instanceof HTMLInputElement || value instanceof HTMLTextAreaElement
   );
 }
 
-export default function cheeze(input: DOMElements): InputElementObservable;
-export default function cheeze<T>(input: T): BasicObservable<T>;
-export default function cheeze(input: ObservableValueOptions) {
+export default function cheeze(
+  input: ObservableDOMElements,
+  options?: ObservableOptions
+): InputElementObservable;
+export default function cheeze<T>(
+  input: T,
+  options?: ObservableOptions
+): BasicObservable<T>;
+export default function cheeze<T extends ObservableValueOptions>(
+  input: T,
+  options?: ObservableOptions
+) {
   if (isDOMInputElement(input)) {
     return new InputElementObservable(input);
   } else {
-    return new BasicObservable(input);
+    return new BasicObservable<T>(input, options);
   }
 }
